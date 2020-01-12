9864f4aa09422f6edfbccd9f875bc1e9125c462f
Hunkly <vityasapozhnik1998@gmail.com>
2020-01-12 16:06:21 +0200

    Update route logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,48 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import { Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import Container from './shared/components/Container';
 import Header from './shared/components/Header';
 import UserPage from './pages/UserPage';
 import GreetingPage from './pages/GreetingPage';
 import AuthorizationPage from './pages/AuthorizationPage';
 import RegistrationPage from "./pages/RegistrationPage";
+import NewsPage from "./pages/NewsPage";
 import pathHistory from "./pathHistory";
 import './App.css';
 import Store from './store/store';
 
-const News = () => <p>News</p>;
 const About = () => <p>About Us</p>;
-
+const userLocalData = JSON.parse(localStorage.getItem('state') || "{}");
 
 const App: React.FC = () => {
 
   return (
-      <Provider store={Store}>
-        <Router history={pathHistory}>
-         <Header/>
-           <Container>
-            <Switch>
-             <Route exact={true} path="/" component={GreetingPage} />
-             <Route path="/news" component={News} />
-             <Route path="/about-us" component={About} />
-             <Route path="/users/:id" component={UserPage} />
-             <Route path="/auth" component={AuthorizationPage} />
-             <Route path="/signup" component={RegistrationPage} />
-            </Switch>
-           </Container>
-        </Router>
-      </Provider>
+      <div className='App'>
+          <Provider store={Store}>
+            <Router history={pathHistory}>
+             <Header/>
+               <Container>
+                    {
+                        userLocalData.isLogged ?
+                            <Switch>
+                                <Route exact={true} path="/news" component={NewsPage} />
+                                <Route path="/about-us" component={About} />
+                                <Route path="/users/:id" component={UserPage} />
+                                <Redirect to='/news' />
+                            </Switch>
+                             :
+                            <Switch>
+                                <Route exact={true} path="/" component={GreetingPage} />
+                                <Route path="/auth/login" component={AuthorizationPage} />
+                                <Route path="/auth/registration" component={RegistrationPage} />
+                                <Redirect to='/' />
+                            </Switch>
+                    }
+               </Container>
+            </Router>
+          </Provider>
+      </div>
   );
 };
 
